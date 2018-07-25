68b43bead356386b0e079be76b7ef2b969aef2a5
hoda5 <josefernandotolentino@gmail.com>
2018-07-25 10:08:49 -0300

    criação projeto

---
+++ packages/@hoda5/tswpvsdobro/index.ts
@@ -0,0 +1,4 @@
+import {soma} from "@hoda5/tswpvssoma"
+function dobro(x: number): number {
+  return soma(x, x);
+}
---
+++ packages/@hoda5/tswpvssoma/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Função que soma
+ * @param a primeira parcela
+ * @param b segunda parcela
+ * @returns soma de a e b
+ */
+export function soma(a: number, b: number): number {
+    return a + b;
+  }
